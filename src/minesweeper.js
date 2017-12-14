3cbd0762bb00c33306ce36a17760feadcf3ae7e8
Ryan Kelso <ryan.kelso@lewiscountywa.gov>
2017-12-14 11:16:03 -0800

    dynamically generate the player board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,45 @@
-const printBoard = board => {
-  console.log('Current Board:');
-  console.log(board[0].join(' | '));
-  console.log(board[1].join(' | '));
-  console.log(board[2].join(' | '));
+// function to dynamically generate the player board
+const generatePlayerBoard = (numberOfRows,numberOfColumns) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    //console.log('row ' + (rowIndex + 1));
+    const row = [];
+    for (columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+      row.push(' ');
+    };
+    board.push(row);
+  };
+  return board;
 };
 
-const board = [
-  [' ',' ',' '],
-  [' ',' ',' '],
-  [' ',' ',' ']
-];
+// function to dynamically generate the bomb board
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  const board = [];
+  for (let rowIndex = 0; rowIndex < numberOfRows; rowIndex++){
+    const row = [];
+    for (columnIndex = 0; columnIndex < numberOfColumns; columnIndex++){
+      row.push(null);
+    };
+    board.push(row);
+  };
 
-printBoard(board);
 
-board[0][1] = '1';
-board[2][2] = 'B';
 
-printBoard(board);
+  numberOfBombsPlaced = 0;
+  while (numberOfBombsPlaced < numberOfBombs) {
+    // create a random integer between 0 and the number of rows or columns
+    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+    board[randomRowIndex][randomColumnIndex] = 'B';
+    numberOfBombsPlaced++;
+    // bombs can be placed on top of existing bombs, fix later
+  };
+
+
+
+  return board;
+};
+
+const printBoard = board => {
+
+};
